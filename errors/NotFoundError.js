d79a320cd34824a42be8c143ff3ec188371c4abf
Maria <120128567+mariag4vr1lova@users.noreply.github.com>
2023-09-25 22:16:55 +0300

    Delete errors directory

--- errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-const { HTTP_STATUS_NOT_FOUND } = require('http2').constants;
-
-module.exports = class NotfoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = HTTP_STATUS_NOT_FOUND;
-  }
-};
